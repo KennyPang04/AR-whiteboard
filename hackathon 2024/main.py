189aa91fe8f03a2c3ca8b054b2d002c32fc2ce74
Rico C <143468977+ricoc236@users.noreply.github.com>
2024-11-09 12:45:38 -0500

    tracking with coords

--- hackathon 2024/main.py
+++ hackathon 2024/main.py
@@ -3,12 +3,9 @@ import mediapipe as mp
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
-
-
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 500)
-
 with mp_hands.Hands(
     static_image_mode=False,
     max_num_hands=2,
@@ -26,10 +23,17 @@ with mp_hands.Hands(
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
                     img, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                for id, landmark in enumerate(hand_landmarks.landmark):
+                    h, w, _ = img.shape
+                    cx, cy = int(landmark.x * w), int(landmark.y * h)
+                    
+                    print(f"ID: {id}, X: {cx}, Y: {cy}")
+                    if(id == 8):
+                        pass #will run coordinate thorugh the white 
+                    # cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
         cv2.imshow("Hand Tracking", img)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
