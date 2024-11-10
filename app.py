00ddc364eb4a4871140f121fb718da3749d921d9
Kenny <kpang@buffalo.edu>
2024-11-10 05:33:30 -0500

    Allow web server

--- app.py
+++ app.py
@@ -205,4 +205,4 @@ def start_streams():
 
 if __name__ == '__main__':
     start_streams()
-    socketio.run(app, debug=True, host='0.0.0.0', port=8080)
+    socketio.run(app, debug=True, host='0.0.0.0', port=8080, allow_unsafe_werkzeug=True)
